a60cdbf0e1bfd8b2904e1dbd3f98f24f2fb1a596
Xis <rick-s@hotmail.com>
2020-11-09 21:37:27 -0700

    Getting closer

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const startPrompt = document.querySelector("#start-prompt");
 const questionContainer = document.querySelector("#question-container");
 const questionText = document.querySelector("#question-text");
 const answersDiv = document.querySelector("#answers");
+const displayTimer = document.querySelector("#time");
 const questions = [
   {
     text: "question?",
@@ -55,18 +56,21 @@ const questions = [
     correctIndex: 0,
   },
 ];
-// this lets the program know what questions its on
+// this lets the program know what questions its on display
 let questionIndex = 0;
 // this is the very start of the test and will handle the start of the timer/interval
 startBtn.addEventListener("click", handleStartClick);
 // this checks what answer the user has selected and checks it against the "correctIndex" of each question
 answersDiv.addEventListener("click", handleAnswerClick);
+// this is the value for the time left, I made it global so i can manipulate it when someone gets it wrong.
+let timeLeft = 75;
+// this is there to help move the test after the final answer is submitted
 
-
-function handleStartClick(e) {
+function handleStartClick() {
   startPrompt.style.display = "none";
   questionContainer.style.display = "block";
   renderQuestion();
+  Timer();
 }
 function handleAnswerClick(e) {
   e.preventDefault();
@@ -76,9 +80,18 @@ function handleAnswerClick(e) {
   const userAnswer = e.target.textContent;
   if (userAnswer === correctAnswer) {
   } else {
+    timeLeft -= 10;
+    if (timeLeft < 0) {
+      timeLeft = 0;
+    } else {
+    }
   }
   questionIndex++;
-  renderQuestion();
+  if (questionIndex == questions.length) {
+    testOver();
+  } else {
+    renderQuestion();
+  }
 }
 function renderQuestion() {
   const currentQuestion = questions[questionIndex];
@@ -91,4 +104,17 @@ function renderQuestion() {
     btn.textContent = answer;
     answersDiv.appendChild(btn);
   }
+}
+function Timer() {
+  let timeInterval = setInterval(function () {
+    displayTimer.textContent = timeLeft;
+    timeLeft--;
+    if (timeLeft <= 0) {
+      clearInterval(timeInterval);
+      window.open("highscore.html");
+    }
+  }, 1000);
+}
+function testOver() {
+    
 }
